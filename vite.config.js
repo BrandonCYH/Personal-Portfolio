136a28c40c0f7e1e6f9ddec9d769c6039ea73765
CHENG YANG HO <chengyangho14@gmail.com>
2026-02-05 17:26:00 +0800

    Refactor Vercel configuration and update package.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-Portfolio/', // Change to your repo name
+  base: '/', // Change to your repo name
+   build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
